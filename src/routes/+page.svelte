1730bcdae544be9b0ac6d2664d3ede82cbf13815
mazylol <landonp95@gmail.com>
2022-09-16 22:45:08 -0400

    soon(tm)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
   <p class="text-9xl font-bold text-gray-200 text-center mt-16">Warper</p>
   <p class="text-4xl font-medium text-gray-300 text-center mt-4">A better messenger</p>
   <div
-    class="rounded-md mt-[30vh] px-4 py-4 max-w-7xl mx-auto bg-gradient-to-r from-blue-500 to-green-400"
+    class="rounded-md mt-[30vh] px-4 py-4 max-w-7xl mx-auto bg-gradient-to-r from-red-500 via-orange-500 to-amber-400"
   >
     <p class="text-center text-gray-200 text-5xl">Soon™</p>
   </div>
